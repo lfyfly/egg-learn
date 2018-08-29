7acf3137b2c1b016394c94e670335e707daa46b2
lfyfly <410793635@qq.com>
2018-08-29 17:57:16 +0800

    add: 模板渲染

---
+++ config/config.default.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports = appInfo => {
+  const config = exports = {};
+
+  // use for cookie sign key, should change to your own and keep security
+  config.keys = appInfo.name + '_1535533391737_1367';
+  // 添加 view 配置
+  exports.view = {
+    defaultViewEngine: 'nunjucks',
+    mapping: {
+      '.tpl': 'nunjucks',
+    },
+  };
+  // add your config here
+  config.middleware = [];
+
+  return config;
+};
+
